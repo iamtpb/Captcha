e215d8642ba4207921ecf2f6b268c7b5cec8f560
xipher <Bhole.Tejas@live.com>
2016-06-22 03:04:59 +0530

    Implemented Time Delays ( Temporary Solution) Implemented Checks Fixed JPanels Fixed ImageIcons *Basic Functionality Build Completed*

--- src/Captcha.java
+++ src/Captcha.java
@@ -5,9 +5,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.*;
 
 public class Captcha {
-    int tries = 0;
+    int tries = 0, time = 0;
     JFrame mainWindow;
     JPanel mainPanel;
     JLabel label_title, button_reset, label_msg;
@@ -30,13 +31,16 @@ public class Captcha {
         mainWindow = new JFrame();
         mainWindow.setLocationRelativeTo(null);
         mainWindow.setSize(new Dimension(300, 300));
-
+        mainWindow.setResizable(false);
         mainPanel = new JPanel();
         label_title = new JLabel("Captcha");
         captchaOverlayPanel = new CaptchaOverlayPanel();
         button_submit = new JButton("Submit");
         button_submit.addActionListener(new Event_Submitted()); //Button Handler
-        button_reset = new JLabel("r");
+        button_reset = new JLabel();
+        button_reset.setIcon(new ImageIcon("res//reset.png"));
+
+
         button_reset.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 System.out.println("Reset Key Pressed");
@@ -49,10 +53,11 @@ public class Captcha {
         });
         tf_input = new JTextField();
         tf_input.setColumns(10);
-        tf_input.setMaximumSize(new Dimension(100, 50));
+        tf_input.setMaximumSize(new Dimension(100, 20));
         label_msg = new JLabel("Enter Text:");
 
         JPanel tx_reset = new JPanel();
+        // tx_reset.setMaximumSize(new Dimension(300,20));
         tx_reset.setLayout(new BoxLayout(tx_reset, BoxLayout.X_AXIS));
         tx_reset.add(tf_input);
         tx_reset.add(button_reset);
@@ -64,7 +69,7 @@ public class Captcha {
         tf_input.setAlignmentX(Component.CENTER_ALIGNMENT);
         button_submit.setAlignmentX(Component.CENTER_ALIGNMENT);
         captchaOverlayPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
-
+        tx_reset.setAlignmentX(Component.CENTER_ALIGNMENT);
         //Add Components to the Container
 
         mainPanel.add(label_title);
@@ -81,6 +86,26 @@ public class Captcha {
 
     }
 
+    private void check() {
+        if (tries >= 3) {
+            button_submit.setEnabled(false);
+            time += 10;
+            JOptionPane.showMessageDialog(null, "3 wrong attempts! Please Wait for " + time + " seconds.");
+            try {
+                Thread.sleep(time * 1000);
+                mainPanel.remove(1);
+                captchaOverlayPanel = new CaptchaOverlayPanel();
+                mainPanel.add(captchaOverlayPanel, 1);
+                button_submit.setEnabled(true);
+                mainWindow.revalidate();
+                mainWindow.repaint();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
     private class Event_Submitted implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -90,6 +115,9 @@ public class Captcha {
                 System.out.println("Entered: " + tf_input.getText());
                 if (captchaOverlayPanel.getSecret().equals(tf_input.getText())) {
                     System.out.println("Checked!");
+                } else {
+                    tries++;
+                    check();
                 }
                 //mainWindow.remove(mainPanel);
                 mainWindow.repaint();
